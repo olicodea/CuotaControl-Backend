9d66d91d3d91efba9b720631ba103313c9c76915
void_oli <olicodea@gmail.com>
2024-09-05 02:22:29 -0300

    Mongo - Delete console.log(URI)

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDataBase = async () => {
     try {
         const URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}`;
-        console.log(URI);
+        
         await mongoose.connect(URI);
 
         console.log('MongoDB connected');
