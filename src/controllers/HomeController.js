debf5236fdbfa363ebc8e8222243b6bdf0e2bd7e
Juan Ignacio Oliva <olicodea@gmail.com>
2024-09-08 20:15:27 -0300

    Home - feat Logica de controller y service

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,15 @@
 import { findHomeData } from "../services/HomeService.js";
 
-export const getHomeData = (req, res) => {
+export const getHomeData = async (req, res) => {
+    const userId = req.query.userId || req.headers["userId"];
+
+    if (!userId)
+        return res
+            .status(400)
+            .json({ error: "El ID de usuario es requerido." });
+
     try {
-        const data = findHomeData();
+        const data = await findHomeData(userId);
         res.status(200).json(data);
     } catch (error) {
         res.status(500).json({ error: "Error al obtener los datos del Home" });
--- src/services/HomeService.js
+++ src/services/HomeService.js
@@ -1,34 +1,62 @@
-export const findHomeData = () => {
-    const homeData = {
-      totalDeuda: "500.230,33",
-      totalAFavor: "1.540.230,33",
-      totalDeudaPago: "539.080,62",
-      totalAFavorPago: "235.108,26",
-      vencimientosDeuda: [
-        {
-          nroPrestamo: "1",
-          cuotaNro: "3/6",
-          fechaVencimiento: "25/09/2024"
-        },
-        {
-          nroPrestamo: "2",
-          cuotaNro: "2/6",
-          fechaVencimiento: "26/09/2024"
-        }
-      ],
-      vencimientosAFavor: [
-        {
-          nroPrestamo: "3",
-          cuotaNro: "4/12",
-          fechaVencimiento: "30/09/2024"
-        },
-        {
-          nroPrestamo: "4",
-          cuotaNro: "2/6",
-          fechaVencimiento: "16/09/2024"
-        }
-      ]
-    };
-  
-    return homeData;
-  };
+import Loan from "../models/Loan.js";
+import { LoanTypesReverse } from "../enums/LoanTypes.js";
+import { InstallmentStatusesReverse } from "../enums/InstallmentStatuses.js";
+
+export const findHomeData = async (userId) => {
+
+    try {
+        const loans = await Loan.find({ usuarioId: userId }).populate("cuotas");
+
+        let totalDebt = 0;
+        let totalInFavor = 0;
+        let totalDebtPaid = 0;
+        let totalInFavorPaid = 0;
+        let dueDebts = [];
+        let dueInFavor = [];
+
+        loans.forEach((loan, indexLoan) => {
+            const loanType = LoanTypesReverse[loan.tipoPrestamo];
+
+            loan.cuotas.forEach((installment, indexInstallment) => {
+                const installmentStatus = InstallmentStatusesReverse[installment.estadoCuota];
+                const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+
+                if (loanType === "prestado") {
+                    totalInFavor += loan.montoTotal;
+                    if (installmentStatus === "pagada") {
+                        totalInFavorPaid += installment.montoCuota;
+                    } else {
+                        dueInFavor.push({
+                            nroPrestamo: indexLoan + 1,
+                            cuotaNro: `${indexInstallment + 1}/${loan.cuotas.length}`,
+                            fechaVencimiento: new Date(installment.fechaVencimiento).toLocaleDateString('es-AR', options),
+                        });
+                    }
+                } else {
+                    totalDebt += loan.montoTotal;
+                    if (installmentStatus === "pagada") {
+                        totalDebtPaid += installment.montoCuota;
+                    } else {
+                        dueDebts.push({
+                            nroPrestamo: indexLoan + 1,
+                            cuotaNro: `${indexInstallment + 1}/${loan.cuotas.length}`, 
+                            fechaVencimiento: new Date(installment.fechaVencimiento).toLocaleDateString('es-AR', options),
+                        });
+                    }
+                }
+            });
+        });
+
+        return {
+            totalDeuda: totalDebt,
+            totalAFavor: totalInFavor,
+            totalDeudaPago: totalDebtPaid,
+            totalAFavorPago: totalInFavorPaid,
+            vencimientosDeuda: dueDebts,
+            vencimientosAFavor: dueInFavor,
+        };
+    } catch (error) {
+        console.error("Error al obtener datos del servicio Home:", error);
+        throw new Error("Error al obtener los datos del Home");
+    }
+};
