cf81a289c6a75f69091d6e7f4c1d3f96eeba8697
Juan Ignacio Oliva <olicodea@gmail.com>
2024-10-05 11:17:44 -0300

    Contacts - Feat get contacts by userId

---
+++ src/controllers/ContactsController.js
@@ -0,0 +1,17 @@
+import { findContactsByUserId } from "../services/ContactsService.js";
+
+export const getContacts = async (req, res) => {
+    const userId = req.query.userId || req.headers["userId"];
+
+    if (!userId)
+        return res
+            .status(400)
+            .json({ error: "El ID de usuario es requerido." });
+
+    try {
+        const data = await findContactsByUserId(userId);
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: "Error al obtener los datos del Home" });
+    }
+};
